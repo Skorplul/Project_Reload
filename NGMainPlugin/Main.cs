5718d4ff381b48ce3a34d7e6a6b9d19357d0ecf6
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-11-01 13:46:55 +0100

    preperation for start of eventsys

--- NGMainPlugin/Main.cs
+++ NGMainPlugin/Main.cs
@@ -10,8 +10,6 @@
     using NGMainPlugin.Systems.RespawnTimer;
     using NGMainPlugin.Systems.EventHandlers;
     using NGMainPlugin.Systems.CustomItems;
-    using NGMainPlugin.Systems.Database;
-    using NGMainPlugin.Systems.SCPSwap;
     using NGMainPlugin.Systems.Notifications;
 
     public class NGMainPlguin : Plugin<Config>
@@ -32,13 +30,6 @@
 
             Log.Info("                                                                                                                                                                         \r\n                                                                                                                                                                         \r\n          `7MN.   `7MF`7MM\"\"\"YMM  `YMM'   `MP`7MMF'   `7MF'.M\"\"\"bgd                       .g8\"\"\"bgd       db     `7MMM.     ,MMF`7MMF`7MN.   `7MF' .g8\"\"\"bgd             \r\n            MMN.    M   MM    `7    VMb.  ,P   MM       M ,MI    \"Y                     .dP'     `M      ;MM:      MMMb    dPMM   MM   MMN.    M .dP'     `M             \r\n            M YMb   M   MM   d       `MM.M'    MM       M `MMb.                         dM'       `     ,V^MM.     M YM   ,M MM   MM   M YMb   M dM'       `             \r\n            M  `MN. M   MMmmMM         MMb     MM       M   `YMMNq.                     MM             ,M  `MM     M  Mb  M' MM   MM   M  `MN. M MM                      \r\nmmmmm mmmmm M   `MM.M   MM   Y  ,    ,M'`Mb.   MM       M .     `MM                     MM.    `7MMF'  AbmmmqMA    M  YM.P'  MM   MM   M   `MM.M MM.    `7MMFmmmmm mmmmm \r\n            M     YMM   MM     ,M   ,P   `MM.  YM.     ,M Mb     dM                     `Mb.     MM   A'     VML   M  `YM'   MM   MM   M     YMM `Mb.     MM             \r\n          .JML.    YM .JMMmmmmMMM .MM:.  .:MMa. `bmmmmd\"' P\"Ybmmd\"                        `\"bmmmdPY .AMA.   .AMMA.JML. `'  .JMML.JMML.JML.    YM   `\"bmmmdPY             \r\n                                                                                                                                                                         \r\n                                                                                                                                                                         \r\n\r\n");
 
-            ///throws error; please check @lastpenguin
-            
-            // Load Database
-            Log.Info("Loading Database...");
-            Database.InitDB();
-            
-
             // Load all configs
             Log.Info("Loading Configs...");
             Config.LoadConfigs();
@@ -55,12 +46,6 @@
 
             Log.Info("Enabling Event Handlers...");
             EventHandlers.Enable();
-
-            Log.Info("Enabling SCP Swap..");
-            SCPSwap.Enable();
-
-            Log.Info("Enabling Random Painkillers...");
-            RandomPainkillers.Enable();
 
             Log.Info("Enabling Lobby System...");
             LobbySystem.Enable();
@@ -89,13 +74,6 @@
         {
             Log.Info("-----[NGMainPlugin Disable]-----");
 
-            ///throws error; please check @lastpenguin
-            
-            // Close the DB
-            Log.Info("Closing Database...");
-            Database.CloseBD();
-            
-
             // Unpatch everything
             Log.Info("Harmony Unpatching...");
             Harmony.UnpatchAll();
@@ -108,12 +86,6 @@
 
             Log.Info("Disabling Event Handlers...");
             EventHandlers.Disable();
-
-            Log.Info("Disabling SCP Swap...");
-            SCPSwap.Disable();
-
-            Log.Info("Disabling Random Painkillers...");
-            RandomPainkillers.Disable();
 
             Log.Info("Disabling Lobby System...");
             LobbySystem.Disable();
