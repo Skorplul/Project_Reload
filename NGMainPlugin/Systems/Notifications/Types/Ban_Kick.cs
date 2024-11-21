88d06094c38d98b0db85389a600caa00ebb051cd
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-11-21 22:16:14 +0100

    won't show faild verification

--- NGMainPlugin/Systems/Notifications/Types/Ban_Kick.cs
+++ NGMainPlugin/Systems/Notifications/Types/Ban_Kick.cs
@@ -14,6 +14,10 @@ namespace NGMainPlugin.Systems.Notifications
 
         internal static void OnBanning(BanningEventArgs ev)
         {
+            if (ev.Player == null || ev.Player.Nickname == "")
+            {
+                return;
+            }
             Banned = true;
             Map.Broadcast(10, $"[<color=#f67979>N</color><color=#e86e6c>e</color><color=#d96260>x</color><color=#cb5754>u</color><color=#bd4c48>s</color><color=#af413c>G</color><color=#a13631>a</color><color=#932a26>m</color><color=#851f1b>i</color><color=#771211>n</color><color=#6a0303>g</color>]: {ev.Target.Nickname} has been banned from the server!");
             foreach (Player ply in Player.List)
@@ -28,6 +32,10 @@ namespace NGMainPlugin.Systems.Notifications
 
         internal static void OnKick(KickedEventArgs ev)
         {
+            if (ev.Player == null || ev.Player.Nickname == "")
+            {
+                return;
+            }
             if (!Banned)
             {
                 Map.Broadcast(10, $"[<color=#f67979>N</color><color=#e86e6c>e</color><color=#d96260>x</color><color=#cb5754>u</color><color=#bd4c48>s</color><color=#af413c>G</color><color=#a13631>a</color><color=#932a26>m</color><color=#851f1b>i</color><color=#771211>n</color><color=#6a0303>g</color>]: {ev.Player.Nickname} has been kicked from the server!");
