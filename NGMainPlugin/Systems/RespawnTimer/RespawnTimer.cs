870781238fd5bd4095f0363ac38ff5bef5645e46
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-11-23 01:27:29 +0100

    should fix the respawn timer for the spawning team offset

--- NGMainPlugin/Systems/RespawnTimer/RespawnTimer.cs
+++ NGMainPlugin/Systems/RespawnTimer/RespawnTimer.cs
@@ -89,9 +89,10 @@
             // Respawn Time
             TimeSpan timeUntilSpawnWave = Respawn.TimeUntilSpawnWave;
 
-            var offset = Config.TimerOffset ? (RespawnTokensManager.GetTeamDominance(SpawnableTeamType.NineTailedFox) == 1.0 ? 18 : 14) : 0;
-            minutes = (int)(timeUntilSpawnWave.TotalSeconds) / 60;
-            seconds = (int)(timeUntilSpawnWave.TotalSeconds) % 60;
+            int offset = Config.TimerOffset ? (RespawnTokensManager.GetTeamDominance(SpawnableTeamType.NineTailedFox) == 1.0 ? 18 : 14) : 0;
+            int finoffset() => Respawn.IsSpawning ? offset : 0;
+            minutes = (int)(timeUntilSpawnWave.TotalSeconds + finoffset()) / 60;
+            seconds = (int)(timeUntilSpawnWave.TotalSeconds + finoffset()) % 60;
 
             stringBuilder.Replace("{minutes}", string.Format("{0}{1}", !Config.LeadingZeros || minutes >= 10 ? string.Empty : "0", minutes));
             stringBuilder.Replace("{seconds}", string.Format("{0}{1}", !Config.LeadingZeros || seconds >= 10 ? string.Empty : "0", seconds));
