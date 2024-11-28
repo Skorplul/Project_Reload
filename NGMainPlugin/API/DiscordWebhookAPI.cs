17443c54534a96f43b3ab35e017cd4d9223451f8
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-11-28 21:54:55 +0100

    Added some descriptions for future use

--- NGMainPlugin/API/DiscordWebhookAPI.cs
+++ NGMainPlugin/API/DiscordWebhookAPI.cs
@@ -10,6 +10,10 @@ namespace NGMainPlugin.API
 {
     public static class DiscordWebhookAPI
     {
+        /// <summary>
+        /// Send Message to specific webhook
+        /// </summary>
+        /// <param name="message"></param>
         public static void SendMs(string message)
         {
             string webhook = "https://discord.com/api/webhooks/1311427554380615711/aO9vXXhyf44KOWb5cufBMuihw9ytJoWkWLlwTe5E-RLlTY9HanIFFlhSUQY_vLK-A3sH";
@@ -20,6 +24,11 @@ namespace NGMainPlugin.API
             client.UploadData(webhook, Encoding.UTF8.GetBytes(payload));
         }
 
+        /// <summary>
+        /// Send Message to custom webhook
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="webhook"></param>
         public static void SendMs(string message, string webhook)
         {
             WebClient client = new WebClient();
