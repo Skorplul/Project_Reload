6ce6a37227a7fcdd53ed5bd1f1c5aec0b19817d5
Skorp <105490480+Skorplul@users.noreply.github.com>
2025-03-03 22:21:14 +0100

    fixed scale reset for "fsc"

--- NGMainPlugin/Systems/Subclasses/Guard/Allseeing.cs
+++ NGMainPlugin/Systems/Subclasses/Guard/Allseeing.cs
@@ -9,6 +9,7 @@ public static class Allseeing
     {
         ply.ShowHint("[<color=#180D76>P</color><color=#190D7F>r</color><color=#1A0D88>o</color><color=#1B0D91>j</color><color=#1C0D9A>e</color><color=#1D0DA3>c</color><color=#1E0DAC>t</color> <color=#200DBE>R</color><color=#210DC7>e</color><color=#220DD0>l</color><color=#230DD9>o</color><color=#240DE2>a</color><color=#250DEB>d</color>] Du Bist nun der Alles sehende!");
         ply.ClearInventory();
+        ply.Scale.Set(1, 1, 1);
         ply.GrantLoadout(RoleTypeId.NtfCaptain);
         ply.EnableEffect(Exiled.API.Enums.EffectType.Scp1344);
     }
--- NGMainPlugin/Systems/Subclasses/Guard/Sondereinheit.cs
+++ NGMainPlugin/Systems/Subclasses/Guard/Sondereinheit.cs
@@ -10,6 +10,7 @@ public static class Sondereinheit
             return;
         ply.ShowHint("[<color=#180D76>P</color><color=#190D7F>r</color><color=#1A0D88>o</color><color=#1B0D91>j</color><color=#1C0D9A>e</color><color=#1D0DA3>c</color><color=#1E0DAC>t</color> <color=#200DBE>R</color><color=#210DC7>e</color><color=#220DD0>l</color><color=#230DD9>o</color><color=#240DE2>a</color><color=#250DEB>d</color>] Du Bist nun eine Sondereinheit!");
         ply.ClearInventory();
+        ply.Scale.Set(1, 1, 1);
         ply.AddItem(ItemType.ParticleDisruptor);
         ply.AddItem(ItemType.GunE11SR);
         ply.AddItem(ItemType.ArmorHeavy);
